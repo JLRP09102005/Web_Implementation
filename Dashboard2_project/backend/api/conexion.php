8f39d6d74cd7adea5602b2492725ead46bfb4926
Jose Luis <jlrp09102005@gmail.com>
2026-03-17 09:20:32 +0100

    base conexion.php

--- Dashboard2_project/backend/api/conexion.php
+++ Dashboard2_project/backend/api/conexion.php
@@ -1,5 +1,20 @@
 <?php 
 
+$host='localhost';
+$dbname='tienda';
+$username='root';
+$password='';
 
+try{
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+
+    //Exception Manager
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOCIATIVO);
+    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+} catch(PDOException $e){
+    die("Connection failed: " . $e->getMessage());
+}
 
 ?>

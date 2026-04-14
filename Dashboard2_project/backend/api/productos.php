a1e70bb579e941f09ea7cba38aa10e7348949343
Jose Luis Ruiz Perez <jlrp09102005@gmail.com>
2026-04-14 12:27:36 +0200

    save

--- Dashboard2_project/backend/api/productos.php
+++ Dashboard2_project/backend/api/productos.php
@@ -39,6 +39,66 @@ try{
         
         case "POST":
             $input = json_decode(file_get_contents("php://input"), true);
+
+            if (!isset($input['nombre']) || !isset($input['precio']) || !isset($input['stock']))
+            {
+                http_response_code(400);
+                echo json_encode(['error' => 'Fields not found']);
+                exit;
+            }
+
+            $sql = 'INSERT INTO productos (nombre, precio, stock) VALUES (:nombre, :precio, :stock)';
+
+            $stmt = $pdo->prepare($sql);
+            $stmt -> execute([':nombre' => $input['nombre'], ':precio' => $input['precio'], ':stock' => $input['stock']]);
+
+            $newID = $pdo->lastInsertId();
+            http_response_code(201);
+            echo json_decode(['id' => $newID, 'mensaje' => 'Registered product succesfuly.']);
+        break;
+
+        case "PUT":
+            
+            if (!isset($_GET['id']))
+            {
+                http_response_code(400);
+                echo json_encode(['error' => 'Product id requiered']);
+                exit;
+            }
+
+            $id = $_GET['id'];
+            $input = json_decode(file_get_contents('php://input',true));
+
+            $fields = [];
+            $parameters = [':id' => $id];
+
+            if (empty($fields))
+            {
+                http_response_code(400);
+                echo json_encode(['mensaje' => 'Not fields sent to update product info']);
+                exit;
+            }
+
+            if (isset($input['nombre']))
+            {
+                $fields[] = 'nombre = :nombre';
+                $parameters[':nombre'] = $input['nombre'];
+            }
+
+            if (isset($input['precio']))
+            {
+                $fields[] = 'precio = :precio';
+                $parameters[':precio'] = $input['precio'];
+            }
+
+            if (isset($input['stock']))
+            {
+                $fields[] = 'stock = :stock';
+                $parameters[':stock'] = $input['stock'];
+            }
+
+            $sql = 'UPDATE productos SET ' . implode('. ', $fields) . "WHERE id = :id";
+
         break;
     }
 }
